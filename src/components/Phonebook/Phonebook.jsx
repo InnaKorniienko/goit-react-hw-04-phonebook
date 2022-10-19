bd0b61e76eab1c5e62a900e92e1d394861c288dd
Inna <inna.kornieko@gmail.com>
2022-10-19 21:11:33 +0300

    Upd file

---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import PhonebookAdd from "./PhonebookAdd";
+import Contactlist from "./Contactlist";
+import Filter from "./Filter";
+import { nanoid } from "nanoid";
+import Notiflix from "notiflix";
+import css from "./Phonebook.module.css";
+
+
+export default function Phonebook() {
+    const [contacts, setContacts] = useState(() => {
+    const value = JSON.parse(localStorage.getItem('contacts'));
+        return value ?? [];
+    });
+    const [filter, setFilter] = useState("");
+
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+    }, [contacts])
+
+const addContact = (contact) => {
+    if (isDublicate(contact)){
+    return Notiflix.Notify.warning(`${contact.name} - ${contact.number} is already in contact`)
+    }
+
+    setContacts((prev) => {
+        const newContact = {
+        id: nanoid(),
+        ...contact
+        }
+        return [...prev, newContact]
+        })
+    }
+    
+const removeContact = (id) => {
+        setContacts((prev) => {
+        const newContacts = prev.filter((item) => item.id !== id);
+        return newContacts
+        })
+    }
+    
+const changeFilter = (e) => {
+    const { value } = e.target;
+        setFilter(value)
+        };
+    
+const isDublicate = ({name, number}) => {
+        const result = contacts.find((item) => item.name === name && item.number === number);
+        return result;
+    }
+
+    
+const getFilteredContacts = () => {
+        if(!filter) {
+        return contacts;
+    }
+    
+        const normalizedFilter = filter.toLocaleLowerCase();
+        const filteredContacts = contacts.filter(({ name, number }) => {
+        const normalizedName = name.toLocaleLowerCase();
+        const normalizedNumber = number.toLocaleLowerCase();
+        const result = normalizedName.includes(normalizedFilter) || normalizedNumber.includes(normalizedFilter);
+        return result;
+        })
+    
+        return filteredContacts;
+    }
+
+const filteredContacts = getFilteredContacts(); 
+
+    return (
+        <div className={css.container}>
+        <div className={css.phonebook}>
+            <h2>Phoneboook</h2>
+            <PhonebookAdd onSubmit={addContact}/>
+        </div>
+        <div className={css.contacts}>
+            <h2>Contacts</h2>
+            <Filter value={filter} onChangeFilter={changeFilter} />
+            <Contactlist items={filteredContacts} removeContact={removeContact}/>
+        </div>
+        </div>
+    )
+}
